24e6a0bff80b7c1db14efd6c6f8e224e870bdfc6
nikhilmulik2002 <nikhilmulik2002@gmail.com>
2017-05-08 10:32:10 -0400

    Files updated with respect to documentstion

--- adroit/website_scrapper.py
+++ adroit/website_scrapper.py
@@ -1,6 +1,6 @@
 import urllib2
 from BeautifulSoup import BeautifulSoup
 
-page = urllib2.urlopen("http://www.trackload.com/cgi-bin/rapidtrk.cgi?MAWB=205-39923332")
+page = urllib2.urlopen("")
 soup = BeautifulSoup(page)
 print soup.prettify()
